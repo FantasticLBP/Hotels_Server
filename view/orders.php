3d70c096116e606b20e59cb18b07869d92376159
704568245@qq.com <704568245@qq.com>
2017-04-15 16:52:39 +0800

    Datables 本地化

--- view/orders.php
+++ view/orders.php
@@ -695,7 +695,22 @@
                 { "bSortable": false },
                 null, null,null, null, null,
                 { "bSortable": false }
-            ] } );
+            ],
+            "oLanguage": {
+                "sLengthMenu": "每页显示 _MENU_ 条记录",
+                "sZeroRecords": "抱歉， 没有找到",
+                "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+                "sInfoEmpty": "没有数据",
+                "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+                "sSearch": "搜索:",
+                "oPaginate": {
+                    "sFirst": "首页",
+                    "sPrevious": "前一页",
+                    "sNext": "后一页",
+                    "sLast": "尾页"
+                },
+                "sZeroRecords": "没有检索到数据"
+            },} );
 
 
         $('table th input:checkbox').on('click' , function(){
